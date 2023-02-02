030c6c77906a4d719c349b79937f5b23a6d44272
KingofTime <antonio_neto77@hotmail.com.br>
2023-02-02 17:32:40 -0300

    Create a rate limit for hash generation route

--- src/Controller/HashController.php
+++ src/Controller/HashController.php
@@ -3,21 +3,30 @@
 namespace App\Controller;
 
 use App\Service\HashManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/hash", name="hash_")
  */
-class HashController
+class HashController extends AbstractController
 {
     /**
      * @Route("/{text}", name="create", methods={"POST"})
      */
-    public function create(String $text): Response
+    public function create(String $text, Request $request, RateLimiterFactory $createHashLimiter): Response
     {
+        $limiter = $createHashLimiter->create($request->getClientIp());
+
+        if(!$limiter->consume(1)->isAccepted()) {
+            throw new TooManyRequestsHttpException();
+        }
+
         $hashManager = new HashManager();
         $hashManager->generate($text);
 
